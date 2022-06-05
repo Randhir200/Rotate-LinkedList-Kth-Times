d675ccaca2ef10a34719a02a20e10eff77740626
Randhir200 <mail.randhirkr@gmail.com>
2022-06-06 00:12:39 +0530

    done

---
+++ main.js
@@ -0,0 +1,160 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  addNodeAtBegin(val) {
+    let node = new Node(val); // {data:20, next:null}
+    if (this.head == null) {
+      this.head = node; //{data:10, next:null}
+      this.tail = node;
+    } else {
+      node.next = this.head; // head = head.next
+      //{data:20, n:{data:10, n:null}}
+      this.head = node; //
+    }
+    this.size++;
+  }
+
+  addNodeAtEnd(val) {
+    let node = new Node(val);
+    if (this.head == null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      let temp = this.head; //
+      while (temp.next != null) {
+        // {data:40, next:null}
+        temp = temp.next; //
+      }
+      //{data:40, next:null}
+      temp.next = node; // {data:40, n:{data:50, next:null}}
+    }
+    this.size++;
+  }
+
+  deleteAtBegin() {
+    if (this.head == null) {
+      console.log('Empty LL');
+      return;
+    } else {
+      // head = {data:40, n:{data:50, next:null}}, head = n:{data:50, next:null}
+      this.head = this.head.next;
+    }
+    this.size--;
+  }
+  deleteAtEnd() {
+    if (this.head == null) {
+      console.log('Empty LL');
+      return;
+    } else {
+      //{data:40, n:{data:50, next:null}} //
+      let temp = this.head;
+      while (temp.next.next != null) {
+        //
+        temp = temp.next;
+      }
+      // {data:40, {data:50, next:nulnext:l}}
+      temp.next = null;
+      this.size--;
+    }
+  }
+  addAtPos(val, pos) {
+    // 80, 2
+    let node = new Node(val);
+    if (this.head == null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      //{data:40, {data:50, next:{data:20,next:{data:30, next:null}}}}
+      let leaderNode = this.head;
+      let nextNode = null;
+      let count = 1; //
+      while (count != pos) {
+        // 2 !=2
+        count++; //2
+        leaderNode = leaderNode.next; // {data:50, next:{data:20,next:{data:30, next:null}}}
+      }
+      nextNode = leaderNode.next; //{data:20,next:{data:30, next:null}}
+      node.next = nextNode; // {data:80, next:{data:20,next:{data:30, next:null}}}
+      leaderNode.next = node; //{data:50, next:{data:80, next:null}
+    }
+    this.size++;
+  }
+
+  deleteAtPos(pos) {
+    //2
+    if (this.head == null) {
+      console.log('Empty');
+      return;
+    } else {
+      //{data:40,n: {data:50, next:{data:20,next:{data:30, next:null}}}}
+      let cur = this.head;
+      let nextNode = null;
+      let count = 1;
+      while (count != pos) {
+        count++;
+        cur = cur.next;
+      }
+      //{data:50, next:{data:20,next:{data:30, next:null}}
+      nextNode = cur.next.next; //{data:30, next:null}
+      cur.next = nextNode;
+    }
+    this.size--;
+  }
+  reverse() {
+    if (this.head == null) {
+      return;
+    } else {
+      let first = this.head; //{data:30, next:{data:20,next:{data:10, next:null}}
+      let second = first.next; //{data:20,next:{data:10, next:null}}
+      //   console.log(second); //{data:20,next:{data:10, next:null}}
+      while (second) {
+        // true, true,
+        let temp = second.next;
+        // console.log(temp);// {data:10, next:null}} || null
+        second.next = first;
+        console.log(second); //{data:20,next:{data:30, next:{data:20,next:{data:10, next:null}}
+        first = second;
+        second = temp;
+      }
+      this.head.next = null;
+      return first;
+    }
+  }
+
+  rotate(k) {
+    let temp = this.head;
+    let n = 0;
+    while (temp.next != null) {
+      temp = temp.next;
+      n++;
+    }
+    temp.next = this.head;
+    k = k%n
+    let jump = n - k;
+    temp = this.head;
+    while (jump) {
+      temp = temp.next;
+      jump--;
+    }
+    let returnHead = temp.next;
+    temp.next = null;
+    return returnHead;
+  }
+}
+
+let ll1 = new LinkedList();
+ll1.addNodeAtBegin(10);
+ll1.addNodeAtBegin(20);
+ll1.addNodeAtBegin(30);
+ll1.addNodeAtBegin(40);
+console.log(ll1.rotate(2));
